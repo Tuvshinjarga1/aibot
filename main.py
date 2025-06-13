849afdfe8d1bead2dc8025177c8882f8f4064a39
Tuvshinjargal Enkhtaivan <tuvshinjargal@fibo.cloud>
2025-06-13 14:22:08 +0800

    test

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ async def webhook(request: Request, token):
 
         url = https://app.chatwoot.com/api/v1/accounts/123470/conversations/5/messages
 
-        data = {"content:" "Hello, bi tanid yj tuslah ve?"}
+        data = {"content": "Hello, bi tanid yj tuslah ve?"}
 
-        headers = {api_access_token: token}
+        headers = {"api_access_token": "Go61PtbAmeXrmmQineSiQyv3"}
 
         response = requests.post(url, json=data, headers=headers)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0", port=8000, reload=True)
